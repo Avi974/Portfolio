7e25cb0da5619ae0365a159298010ce0ab0292ee
Abhik Anand <abhikanand7777@gmail.com>
2025-08-15 14:54:06 +0530

    project setup

---
+++ LICENSE.md
@@ -0,0 +1,7 @@
+Copyright (c) 2025 Abhik Anand
+
+All Rights Reserved.
+
+Permission is hereby granted to view the source code and fork this repository for the sole purpose of educational review and to assess my skills.
+
+No permission is granted to copy, modify, distribute, or sublicense the code for any other purpose, including, but not limited to, its use in other portfolios or projects, whether for commercial or non-commercial use.
---
+++ next.config.mjs
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    unoptimized: true,
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  devIndicators: false, 
+  reactStrictMode: true,
+}
+
+export default nextConfig
